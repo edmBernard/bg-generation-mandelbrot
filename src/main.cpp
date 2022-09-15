e3f56cba8e4433740387929bb58bd9de72b57a3b
edmbernard <bernard.erwan@gmail.com>
2022-09-15 13:31:51 +0200

    feat: add example with different API

--- src/main.cpp
+++ src/main.cpp
@@ -6,55 +6,46 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
 
-// std::string readFile(const char *filePath) {
-//   std::string content;
-//   std::ifstream fileStream(filePath, std::ios::in);
 
-//   if(!fileStream.is_open()) {
-//       std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
-//       return "";
-//   }
+std::string readFile(const char *filePath) {
+  std::string content;
+  std::ifstream fileStream(filePath, std::ios::in);
 
-//   std::string line = "";
-//   while(!fileStream.eof()) {
-//       std::getline(fileStream, line);
-//       content.append(line + "\n");
-//   }
+  if(!fileStream.is_open()) {
+      std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
+      return "";
+  }
 
-//   fileStream.close();
-//   return content;
-// }
+  std::string line = "";
+  while(!fileStream.eof()) {
+      std::getline(fileStream, line);
+      content.append(line + "\n");
+  }
+
+  fileStream.close();
+  return content;
+}
 
 static const struct
 {
   float x, y;
   float r, g, b;
-} vertices[3] =
+} vertices[4] =
     {
-        {-0.6f, -0.4f, 1.f, 0.f, 0.f},
-        {0.6f, -0.4f, 0.f, 1.f, 0.f},
-        {0.f, 0.6f, 0.f, 0.f, 1.f}};
+        {0.f, 0.f, 1.f, 0.f, 0.f},
+        {0.f, 1.f, 0.f, 1.f, 0.f},
+        {1.f, 1.f, 0.f, 0.f, 1.f},
+        {1.f, 0.f, 1.f, 0.f, 0.f}};
 
-static const char *vertex_shader_text =
-    "#version 110\n"
-    "uniform mat4 MVP;\n"
-    "attribute vec3 vCol;\n"
-    "attribute vec2 vPos;\n"
-    "varying vec3 color;\n"
-    "void main()\n"
-    "{\n"
-    "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-    "    color = vCol;\n"
-    "}\n";
+static const std::vector<uint16_t> indices = {
+      0, 1, 2,
+      1, 2, 3};
 
-static const char *fragment_shader_text =
-    "#version 110\n"
-    "varying vec3 color;\n"
-    "void main()\n"
-    "{\n"
-    "    gl_FragColor = vec4(color, 1.0);\n"
-    "}\n";
 
 static void error_callback(int error, const char *description) {
   fprintf(stderr, "Error: %s\n", description);
@@ -67,7 +58,7 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
 
 int main(void) {
   GLFWwindow *window;
-  GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+  GLuint vertex_buffer, indices_buffer, vertex_shader, fragment_shader, program;
   GLint mvp_location, vpos_location, vcol_location;
 
   glfwSetErrorCallback(error_callback);
@@ -95,13 +86,20 @@ int main(void) {
   glGenBuffers(1, &vertex_buffer);
   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glGenBuffers(1, &indices_buffer);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLushort)*indices.size(), indices.data(), GL_STATIC_DRAW);
 
   vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+  auto vertex_shader_text = readFile("../src/vertex_shader.glsl");
+  const char* vertex_shader_data = vertex_shader_text.c_str();
+  glShaderSource(vertex_shader, 1, &vertex_shader_data, NULL);
   glCompileShader(vertex_shader);
 
   fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+  auto fragment_shader_text = readFile("../src/fragment_shader.glsl");
+  const char* fragment_shader_data = fragment_shader_text.c_str();
+  glShaderSource(fragment_shader, 1, &fragment_shader_data, NULL);
   glCompileShader(fragment_shader);
 
   program = glCreateProgram();
@@ -114,11 +112,9 @@ int main(void) {
   vcol_location = glGetAttribLocation(program, "vCol");
 
   glEnableVertexAttribArray(vpos_location);
-  glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                        sizeof(vertices[0]), (void *)0);
+  glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void *)0);
   glEnableVertexAttribArray(vcol_location);
-  glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                        sizeof(vertices[0]), (void *)(sizeof(float) * 2));
+  glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void *)(sizeof(float) * 2));
 
   while (!glfwWindowShouldClose(window)) {
     float ratio;
@@ -138,7 +134,9 @@ int main(void) {
 
     glUseProgram(program);
     glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat *)mvp);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_QUADS, 0, 4);
+    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);  // Use index buffer registered
 
     glfwSwapBuffers(window);
     glfwPollEvents();
